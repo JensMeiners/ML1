e35baeb2b3c44aa9ca3bfd61f4413f7c06316dd8
flix <fehlxb@gmail.com>
2013-10-18 13:35:50 +0200

    Solve 3 b)

--- Ex01/learning.py
+++ Ex01/learning.py
@@ -18,7 +18,8 @@ def plot(X,T):
 def learn(X,T):
 	classify = classifiers.B
 
-	params = []
+	params = [(np.array([w1, w2]), b) for w1 in range(-10,10)
+					for w2 in range(-10,10) for b in range(-10,10)]
 	# TODO: complete this list with more possible parameters
 	#       hint:
 	#        use nested loops to generate a large number
